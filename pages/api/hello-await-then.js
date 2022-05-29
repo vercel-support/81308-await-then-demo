5da624fd580c23d152894234376aabeac6370a8b
William Li <email@williamli.dev>
2022-05-29 23:11:53 +0800

    testing

--- pages/api/hello-await-then.js
+++ pages/api/hello-await-then.js
@@ -4,9 +4,18 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const generatePassword = async (delay) => {
+  await sleep(delay)
+  return {body: "Work-Quiet-Slept-Separate-8"}
+}
+
+const getPassword = async () => {
+  return await getPassword(5000).then(res => res.body)
+}
+
 export default async function handler(req, res) {
-  await sleep(5000).then(()=>{
-    res.status(200).json({ name: 'John Doe' })
-  })
-  res.status(200).json({ name: 'John Wick' })
+  const password = getPassword()
+  console.log({password})
+
+  res.status(200).json({ name: 'John Doe' })
 }
